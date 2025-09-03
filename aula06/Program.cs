f2490320263dbfc71c4931047f4bfb6f61e4d235
Allyssan Marie <allyssan19@gmail.com>
2025-09-03 15:11:00 -0300

    if else

---
+++ aula06/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Aula06;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("------Verificador de Números Pares ou Ímpares------");
+        Console.WriteLine("Digite um numero: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        int result = number % 2;  //Se o resto for 0 é par, senão ímpar
+
+        if (result == 0)
+        {
+            Console.WriteLine($"\nO numero digitado é par ");
+        }
+
+        else
+        {
+            Console.WriteLine("\nO numero digitado é ímpar");
+        }
+    }
+}
