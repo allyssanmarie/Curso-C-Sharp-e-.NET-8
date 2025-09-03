7821c8f00876a4f10a599aac4255a057aea9ee8e
Allyssan Marie <allyssan19@gmail.com>
2025-09-02 23:42:48 -0300

    Variáveis

---
+++ aula02/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Aula02;
+
+public class Program
+{
+    public static void Main()
+    {
+        string myName = "Allyssan";  //Variáveis
+        int number = 31;
+        double numberDouble = 10.50;
+        char character = 'a';  //aspas simples
+        bool boolean = true;
+
+        Console.WriteLine(myName);
+        Console.WriteLine(number);
+        Console.WriteLine(numberDouble);
+        Console.WriteLine(character);
+        Console.WriteLine(boolean);
+    }
+}
