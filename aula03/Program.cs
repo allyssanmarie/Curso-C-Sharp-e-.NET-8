c9aa19354a2a63d8db71f46f01ad4ffe461aebea
Allyssan Marie <allyssan19@gmail.com>
2025-09-03 00:06:23 -0300

    Constante

---
+++ aula03/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Aula03;
+
+public class Program
+{
+    public static void Main()
+    {
+        int number = 30;
+        number = 20;
+
+        Console.WriteLine($"O valor antigo da variável era: {number}");
+
+        number = 10;
+
+        Console.WriteLine($"O valor novo da variável é: {number}");
+
+        const int number1 = 1000;  //Constante o valor nao pode ser redeclarado
+
+        Console.WriteLine($"O valor da constante é: {number1}");
+
+        //No var sempre considera a primeira declaração e no dinâmico o último  // var / dynamic
+    }
+}
